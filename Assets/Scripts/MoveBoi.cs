5ddf60aa7628ed1ef870c2faac499b40a6a84b19
glory20h <glory20h@naver.com>
2020-03-21 15:53:07 +0900

    버그 Fix + Theme1FixedTile1 없었는데 추가

--- Assets/Scripts/MoveBoi.cs
+++ Assets/Scripts/MoveBoi.cs
@@ -138,7 +138,7 @@ public class MoveBoi : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "FixedTile")
+        if (collision.tag == "Tile")
         {
             if (warpDone)           //워프에서 반대편으로 나왔을때 바로 타일 탐지하는것 방지
             {
