55ce3d8319af6d3660306193af0ec6b3f16aaed0
JunHanZang <zang123@unist.ac.kr>
2020-03-24 12:14:41 +0900

    tutorial fin
    finn

--- Assets/TutorialAnimation.cs
+++ Assets/TutorialAnimation.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class TutorialAnimation : MonoBehaviour
 {
+    public Transform target;
     Vector3 targetPosition;
     float speed = 1.0f;
+    bool goBack = false;
     // Start is called before the first frame update
     void Start()
     {
-        targetPosition = new Vector3(75, 75, 0);
-        float step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+        targetPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        if(goBack)
+        {
+            transform.position = targetPosition;
+            goBack = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "position")
+        {
+            goBack = true;
+        }
     }
 }
