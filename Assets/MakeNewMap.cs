044277568da16e4b4b6a984d33f8008d5c3146af
JunHan <fightzuzak@naver.com>
2020-04-27 13:32:18 +0900

    newmap
    여기 부분 추가하기, slack에 해둔거 추가하기

--- Assets/MakeNewMap.cs
+++ Assets/MakeNewMap.cs
@@ -86,11 +86,20 @@ public class MakeNewMap : MonoBehaviour
 
     public void makeNewlevel()
     {
+        if(Newmap == null)//이부분 중요했음 ㅋㅋ 기존 맵 Destoryer
+        {
+
+        }
+        else
+        {
+            Newmap = null;
+            Debug.Log(Newmap);
+        }
         BoardWidth = Random.Range(3, 5);
         BoardHeight = Random.Range(3, 5);
         int scalesizechanger = 0;
         scalesizechanger = (BoardWidth + BoardHeight) / 2;
-        switch(BoardHeight)
+        switch (BoardHeight)
         {
             case 1:
             case 2:
@@ -104,16 +113,16 @@ public class MakeNewMap : MonoBehaviour
                 scaleSize = 3;
                 break;
         }
-        BoyPos = Random.Range(0, BoardHeight-1);
-        GirlPos = Random.Range(0, BoardHeight-1);
+        BoyPos = Random.Range(0, BoardHeight - 1);
+        GirlPos = Random.Range(0, BoardHeight - 1);
         int tilevalue = 0;
         int rangeoftile = 9;
         bool tile8yes = true;//tile8yes 없애면 tile9 1개랑 나머지는 tile8개로 나옴
         bool tile9yes = true;
-            for (int i = 0; i < BoardHeight; i++)
-                for (int j = 0; j < BoardWidth; j++)
-                {
-                    tilevalue = Random.Range(1, rangeoftile);// 1부터
+        for (int i = 0; i < BoardHeight; i++)
+            for (int j = 0; j < BoardWidth; j++)
+            {
+                tilevalue = Random.Range(1, rangeoftile);// 1부터
                 if (tilevalue == 8 && tile8yes)
                 {
                     if (tile9yes)
@@ -128,36 +137,36 @@ public class MakeNewMap : MonoBehaviour
                     }
                 }
                 //타일 Exception
-                 if(j == 0)//<-
-                {
-                    while(tilevalue == 2)
-                    {
-                        tilevalue = Random.Range(1, rangeoftile);
-                    }
-                }
-                 else if(i == 0)
-                {
-                    while (tilevalue == 5)
-                    {
-                        tilevalue = Random.Range(1, rangeoftile);
-                    }
-                }
-                 else if(j == BoardHeight-1)
-                {
-                    while (tilevalue == 4)
-                    {
-                        tilevalue = Random.Range(1, rangeoftile);
-                    }
-                }
-                 else if(j==GirlPos&&i==BoardWidth-1)
+                if (j == 0)//<-
                 {
                     while (tilevalue == 2)
                     {
                         tilevalue = Random.Range(1, rangeoftile);
                     }
                 }
-                    Newmap += tilevalue;
+                else if (i == 0)
+                {
+                    while (tilevalue == 5)
+                    {
+                        tilevalue = Random.Range(1, rangeoftile);
+                    }
                 }
+                else if (j == BoardHeight - 1)
+                {
+                    while (tilevalue == 4)
+                    {
+                        tilevalue = Random.Range(1, rangeoftile);
+                    }
+                }
+                else if (j == GirlPos && i == BoardWidth - 1)
+                {
+                    while (tilevalue == 2)
+                    {
+                        tilevalue = Random.Range(1, rangeoftile);
+                    }
+                }
+                Newmap += tilevalue;
+            }
         BoardEmptyTileTypeInfo = ConvertStringToIntArray(Newmap); // 이걸 통해 만들어서 확인후에 돌아가는지 확인하고 pieces 나누기로 합시당.
     }
 
@@ -195,7 +204,7 @@ public class MakeNewMap : MonoBehaviour
 
     void Checkingsame()
     {
-        if(check == Newmap)
+        if (check == Newmap)
         {
             if (GirlPos == Girlposcheck && BoyPos == Boyposcheck)
                 LoadLevelData(0);
@@ -206,5 +215,49 @@ public class MakeNewMap : MonoBehaviour
             Girlposcheck = GirlPos;
             Boyposcheck = BoyPos;
         }
+    }
+
+    void passivemaker()
+    {
+        BoardWidth = 2; // 2*2타일같이 여기서 수정함
+        BoardHeight = 2;
+
+        //건들지마라
+        int scalesizechanger = 0;
+        scalesizechanger = (BoardWidth + BoardHeight) / 2;
+        switch (BoardHeight)
+        {
+            case 1:
+            case 2:
+            case 3:
+                scaleSize = 1;
+                break;
+            case 4:
+                scaleSize = 2;
+                break;
+            case 5:
+                scaleSize = 3;
+                break;
+        }
+
+        //여자애 남자애 포지션
+        BoyPos = 0;
+        GirlPos = 0;
+
+        int tilevalue = 0;
+        for (int i = 0; i < BoardHeight; i++)
+            for (int j = 0; j < BoardWidth; j++)
+            {
+                tilevalue = 1;
+            }
+
+        /* 다음에 할거
+         Pin = 0000
+print (Pin)
+while Pin < 10000:
+    print (Pin)
+    Pin = Pin + 1
+         */
+        BoardEmptyTileTypeInfo = ConvertStringToIntArray(Newmap);
     }
 }
