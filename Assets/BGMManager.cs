00f8b12328d0a84f455ddb75b714853fa9fad75a
glory20h <56900470+glory20h@users.noreply.github.com>
2021-05-01 19:41:58 +0900

    5/1 RandomBGM 작업 초큼

---
+++ Assets/BGMManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMManager : MonoBehaviour
+{
+    static AudioSource audioSrc;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
