c52e5c3fa3f37c33009514e2860b44d7fd30d061
glory20h <56900470+glory20h@users.noreply.github.com>
2021-05-08 21:28:14 +0900

    5/8 BGM 작업

---
+++ Assets/Scripts/BGMManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMManager : MonoBehaviour
+{
+    public static AudioClip BGMClip;
+    static AudioSource audioSrc;
+
+    int repeatAmount = 3;
+    int currentRepeat = 0;
+
+    void Start()
+    {
+        LoadAudioClip();
+
+        audioSrc = GetComponent<AudioSource>();
+        audioSrc.clip = BGMClip;
+        audioSrc.Play();
+    }
+
+    void Update()
+    {
+        if (!audioSrc.isPlaying)
+        {
+            if (currentRepeat < repeatAmount)
+            {
+                audioSrc.Play();
+                currentRepeat++;
+            }
+            else
+            {
+                LoadAudioClip();
+            }
+        }
+    }
+
+    void LoadAudioClip()
+    {
+        currentRepeat = 0;
+
+        // If Level 1,2 단계, Load from folder 12
+        int r = Random.Range(1, 13);
+        BGMClip = Resources.Load<AudioClip>("Audio/Bpm90/12/" + r.ToString());
+
+        // If Level 3,4 단계, Load from folder 34
+    }
+}
