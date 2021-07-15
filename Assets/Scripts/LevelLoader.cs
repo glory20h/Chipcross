f553173f7f9648f0fcf53d2ab690e25f5e55dba0
JunHanZang <zang123@unist.ac.kr>
2021-07-16 00:13:35 +0900

    Loading
    gg

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -1,23 +1,34 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class LevelLoader : MonoBehaviour
 {
     public Animator transition;
-
+    public GameObject loadingScreen;
+    public GameObject sliderob;
+    public Slider slider;
     public void LoadNextLevel()
     {
-        StartCoroutine(LoadLevel());
+        StartCoroutine(LoadLevel(1));
     }
-
-    IEnumerator LoadLevel()
+    IEnumerator LoadLevel(int sceneIndex)
     {
         transition.SetTrigger("Start");
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
 
-        yield return new WaitForSeconds(0.75f);
-
-        SceneManager.LoadScene(1);
+        loadingScreen.SetActive(false);
+        sliderob.SetActive(true);
+        //Time.timeScale = 0f;
+        //Debug.Log(operation.isDone);
+        //Debug.Log(operation.progress);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            yield return null;
+            //Debug.Log(operation.progress);
+        }
+        //SceneManager.LoadScene(1);
     }
 }
