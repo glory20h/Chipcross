8ff71dd1f0f501050bf622fcfe30cca091522fa3
glory20h <56900470+glory20h@users.noreply.github.com>
2021-12-09 19:59:51 +0900

    12/9 별 애니메이션 추가

--- Assets/BGAnimate.cs
+++ Assets/BGAnimate.cs
@@ -14,6 +14,8 @@ public class BGAnimate : MonoBehaviour
     float speed2;
     float speed3;
     Vector3 scaleChange1;
+    Vector3 scaleChange2;
+    Vector3 scaleChange3;
 
     int level;
 
@@ -23,6 +25,8 @@ public class BGAnimate : MonoBehaviour
         speed2 = 0.16f;
         speed3 = 0.04f;
         scaleChange1 = new Vector3(-0.005f, -0.005f, 0);
+        scaleChange2 = new Vector3(0.02f, 0.02f, 0);
+        scaleChange3 = new Vector3(0.003f, 0.003f, 0);
     }
 
     // Update is called once per frame
@@ -34,11 +38,11 @@ public class BGAnimate : MonoBehaviour
             cloud2.Translate(Vector3.left * Time.deltaTime * speed2);
             if (cloud1.position.x < -12)
             {
-                cloud1.position = new Vector3(12.5f, cloud1.position.y, cloud1.position.z); ;
+                cloud1.position = new Vector3(12.5f, cloud1.position.y, cloud1.position.z);
             }
             if (cloud2.position.x < -11)
             {
-                cloud2.position = new Vector3(11.5f, cloud2.position.y, cloud2.position.z); ;
+                cloud2.position = new Vector3(11.5f, cloud2.position.y, cloud2.position.z);
             }
         }
         if (level == 2)
@@ -48,7 +52,21 @@ public class BGAnimate : MonoBehaviour
             {
                 scaleChange1 = -scaleChange1;
             }
+
+            star_ring.localScale += scaleChange2;
+            star_ring.GetComponent<SpriteRenderer>().color += new Color(0, 0, 0, -0.015f);
+            if(star_ring.localScale.x >= 2.5f)
+            {
+                star_ring.localScale = new Vector3(1, 1, 1);
+                star_ring.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+                star2.localScale = new Vector3(1, 1, 1);
+            }
+
             // Animate Star & Star ring
+            if(star2.localScale.x < 1.1f)
+            {
+                star2.localScale += scaleChange3;
+            }
         }
         if (level == 4)
         {
@@ -68,6 +86,8 @@ public class BGAnimate : MonoBehaviour
         else if (levelDFactor < 0f)
         {
             star1.gameObject.SetActive(true);
+            star2.gameObject.SetActive(true);
+            star_ring.gameObject.SetActive(true);
             level = 2;
         }
         else if (levelDFactor < 0.5f)
