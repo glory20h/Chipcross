83ab54d47b3aad6f9df5002cb3833a1748d83255
glory20h <glory20h@naver.com>
2022-02-13 23:05:45 +0900

    2/13 BG Animation 구현3

--- Assets/BGAnimate.cs
+++ Assets/BGAnimate.cs
@@ -9,18 +9,27 @@ public class BGAnimate : MonoBehaviour
     public Transform meteor1;
     public Transform meteor2;
 
-    public Transform star1;
+    public Transform star1_2;
     public Transform starRing;
-    public Transform star2;
-    public Transform star3;
-    public Transform star4;
+    public Transform star2_2;
+    public Transform star3_2;
+    public Transform star4_2;
     public Transform wave_2;
     public Transform planet_2;
 
     Vector3 star1ScaleChange;
     Vector3 star1RingScaleChange;
 
-
+    public Transform star1_3;
+    public Transform star2_3;
+    public Transform star3_3;
+    public Transform star4_3;
+    public Transform star5_3;
+    public Transform wave1_3;
+    public Transform wave2_3;
+    public Transform wave3_3;
+    public Transform planet_3;
+    public Transform planet_light_3;
 
     public Transform stars;
 
@@ -35,28 +44,42 @@ public class BGAnimate : MonoBehaviour
     public float meteor2Speed = 1.5f;
 
     [Header("- Level 2")]
-    public float star1Scale = 0.005f;
-    public float star1RingScale = 0.015f;
-    public float star2AlphaChange = 0.018f;
-    public float star3AlphaChange = 0.016f;
-    public float star4AlphaChange = 0.017f;
+    public float star1_2_Scale = 0.005f;
+    public float star1_2_RingScale = 0.015f;
+    public float star2_2_AlphaChange = 0.018f;
+    public float star3_2_AlphaChange = 0.016f;
+    public float star4_2_AlphaChange = 0.017f;
 
     public float wave2Speed = 0.1f;
 
     [Header("- Level 3")]
-
+    public float star1_3_AlphaChange = 0.018f;
+    public float star2_3_AlphaChange = 0.018f;
+    public float star3_3_AlphaChange = 0.018f;
+    public float star4_3_AlphaChange = 0.018f;
+    public float star5_3_AlphaChange = 0.018f;
+    public float plantlight_3_AlphaChange = 0.018f;
+    public float wave1_3_Speed = 0.1f;
+    public float wave2_3_Speed = 0.1f;
+    public float wave3_3_Speed = 0.1f;
 
     [Header("- Level 4")]
     public float starsSpeed = 0.015f;
 
     void Start()
     {
-        star1ScaleChange = new Vector3(star1Scale, star1Scale, 0);
-        star1RingScaleChange = new Vector3(star1RingScale, star1RingScale, 0);
+        star1ScaleChange = new Vector3(star1_2_Scale, star1_2_Scale, 0);
+        star1RingScaleChange = new Vector3(star1_2_RingScale, star1_2_RingScale, 0);
 
-        star2.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, Random.Range(0f, 1f));
-        star3.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, Random.Range(0f, 1f));
-        star4.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, Random.Range(0f, 1f));
+        star2_2.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, Random.Range(0f, 1f));
+        star3_2.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, Random.Range(0f, 1f));
+        star4_2.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, Random.Range(0f, 1f));
+
+        star1_3.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, Random.Range(0f, 1f));
+        star2_3.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, Random.Range(0f, 1f));
+        star3_3.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, Random.Range(0f, 1f));
+        star4_3.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, Random.Range(0f, 1f));
+        star5_3.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, Random.Range(0f, 1f));
     }
 
     // Update is called once per frame
@@ -87,7 +110,7 @@ public class BGAnimate : MonoBehaviour
             {
                 meteor1.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, (6f + meteor1.localPosition.y) / (-1.5f - (-6f)));
             }
-            if (meteor1.localPosition.y < -200f)
+            if (meteor1.localPosition.y < -100f)
             {
                 meteor1.localPosition = new Vector3(0, 3f, 0);
             }
@@ -101,7 +124,7 @@ public class BGAnimate : MonoBehaviour
             {
                 meteor2.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, (4f + meteor2.localPosition.y) / (0 - (-4f)));
             }
-            if (meteor2.localPosition.y < -500f)
+            if (meteor2.localPosition.y < -250f)
             {
                 meteor2.localPosition = new Vector3(0, 3f, 0);
             }
@@ -110,16 +133,16 @@ public class BGAnimate : MonoBehaviour
         if (level == 2)
         {
             // Animate Star & Star ring
-            star1.localScale += star1ScaleChange;
+            star1_2.localScale += star1ScaleChange;
             starRing.localScale += star1RingScaleChange;
             starRing.GetComponent<SpriteRenderer>().color += new Color(0, 0, 0, -0.02f);
-            if (star1.localScale.x < 0.5f)
+            if (star1_2.localScale.x < 0.5f)
             {
                 star1ScaleChange = -star1ScaleChange;
                 starRing.localScale = new Vector3(0.7f, 0.7f, 1);
                 starRing.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
             }
-            else if (star1.localScale.x > 0.7f)
+            else if (star1_2.localScale.x > 0.7f)
             {
                 star1ScaleChange = -star1ScaleChange;
             }
@@ -135,22 +158,22 @@ public class BGAnimate : MonoBehaviour
             }
             */
 
-            star2.GetComponent<SpriteRenderer>().color += new Color(0, 0, 0, -star2AlphaChange);
-            if (star2.GetComponent<SpriteRenderer>().color.a >= 1f || star2.GetComponent<SpriteRenderer>().color.a <= 0)
+            star2_2.GetComponent<SpriteRenderer>().color += new Color(0, 0, 0, -star2_2_AlphaChange);
+            if (star2_2.GetComponent<SpriteRenderer>().color.a >= 1f || star2_2.GetComponent<SpriteRenderer>().color.a <= 0)
             {
-                star2AlphaChange = -star2AlphaChange;
+                star2_2_AlphaChange = -star2_2_AlphaChange;
             }
 
-            star3.GetComponent<SpriteRenderer>().color += new Color(0, 0, 0, -star3AlphaChange);
-            if (star3.GetComponent<SpriteRenderer>().color.a >= 1f || star3.GetComponent<SpriteRenderer>().color.a <= 0)
+            star3_2.GetComponent<SpriteRenderer>().color += new Color(0, 0, 0, -star3_2_AlphaChange);
+            if (star3_2.GetComponent<SpriteRenderer>().color.a >= 1f || star3_2.GetComponent<SpriteRenderer>().color.a <= 0)
             {
-                star3AlphaChange = -star3AlphaChange;
+                star3_2_AlphaChange = -star3_2_AlphaChange;
             }
 
-            star4.GetComponent<SpriteRenderer>().color += new Color(0, 0, 0, -star4AlphaChange);
-            if (star4.GetComponent<SpriteRenderer>().color.a >= 1f || star4.GetComponent<SpriteRenderer>().color.a <= 0)
+            star4_2.GetComponent<SpriteRenderer>().color += new Color(0, 0, 0, -star4_2_AlphaChange);
+            if (star4_2.GetComponent<SpriteRenderer>().color.a >= 1f || star4_2.GetComponent<SpriteRenderer>().color.a <= 0)
             {
-                star4AlphaChange = -star4AlphaChange;
+                star4_2_AlphaChange = -star4_2_AlphaChange;
             }
 
             wave_2.Translate(new Vector3(-1, 0) * Time.deltaTime * wave2Speed);
@@ -162,7 +185,59 @@ public class BGAnimate : MonoBehaviour
 
         if (level == 3)
         {
-            
+            star1_3.GetComponent<SpriteRenderer>().color += new Color(0, 0, 0, -star1_3_AlphaChange);
+            if (star1_3.GetComponent<SpriteRenderer>().color.a >= 1f || star1_3.GetComponent<SpriteRenderer>().color.a <= 0)
+            {
+                star1_3_AlphaChange = -star1_3_AlphaChange;
+            }
+
+            star2_3.GetComponent<SpriteRenderer>().color += new Color(0, 0, 0, -star2_3_AlphaChange);
+            if (star2_3.GetComponent<SpriteRenderer>().color.a >= 1f || star2_3.GetComponent<SpriteRenderer>().color.a <= 0)
+            {
+                star2_3_AlphaChange = -star2_3_AlphaChange;
+            }
+
+            star3_3.GetComponent<SpriteRenderer>().color += new Color(0, 0, 0, -star3_3_AlphaChange);
+            if (star3_3.GetComponent<SpriteRenderer>().color.a >= 1f || star3_3.GetComponent<SpriteRenderer>().color.a <= 0)
+            {
+                star3_3_AlphaChange = -star3_3_AlphaChange;
+            }
+
+            star4_3.GetComponent<SpriteRenderer>().color += new Color(0, 0, 0, -star4_3_AlphaChange);
+            if (star4_3.GetComponent<SpriteRenderer>().color.a >= 1f || star4_3.GetComponent<SpriteRenderer>().color.a <= 0)
+            {
+                star4_3_AlphaChange = -star4_3_AlphaChange;
+            }
+
+            star5_3.GetComponent<SpriteRenderer>().color += new Color(0, 0, 0, -star5_3_AlphaChange);
+            if (star5_3.GetComponent<SpriteRenderer>().color.a >= 1f || star5_3.GetComponent<SpriteRenderer>().color.a <= 0)
+            {
+                star5_3_AlphaChange = -star5_3_AlphaChange;
+            }
+
+            wave1_3.Translate(new Vector3(1.778f, -1) * Time.deltaTime * wave1_3_Speed);
+            if (wave1_3.position.x > 3.1f)
+            {
+                wave1_3.position = new Vector3(-14.83f, 1.19f);
+            }
+
+            wave2_3.Translate(new Vector3(1, 0) * Time.deltaTime * wave2_3_Speed);
+            if (wave2_3.position.x > 9.05f)
+            {
+                wave2_3.position = new Vector3(-8.6f, -3.15f);
+            }
+
+            wave3_3.Translate(new Vector3(-1.777f, 1) * Time.deltaTime * wave3_3_Speed);
+            if (wave3_3.position.x < -5.09f)
+            {
+                wave3_3.position = new Vector3(12.818f, -3.674f);
+            }
+
+            planet_light_3.GetComponent<SpriteRenderer>().color += new Color(0, 0, 0, -plantlight_3_AlphaChange);
+            if (planet_light_3.GetComponent<SpriteRenderer>().color.a >= 2f || planet_light_3.GetComponent<SpriteRenderer>().color.a <= 0.25f)
+            {
+                plantlight_3_AlphaChange = -plantlight_3_AlphaChange;
+            }
         }
 
         if (level == 4)
@@ -188,18 +263,27 @@ public class BGAnimate : MonoBehaviour
         }
         else if (levelDFactor < 0f)
         {
-            star1.gameObject.SetActive(true);
+            star1_2.gameObject.SetActive(true);
             starRing.gameObject.SetActive(true);
-            star2.gameObject.SetActive(true);
-            star3.gameObject.SetActive(true);
-            star4.gameObject.SetActive(true);
+            star2_2.gameObject.SetActive(true);
+            star3_2.gameObject.SetActive(true);
+            star4_2.gameObject.SetActive(true);
             wave_2.gameObject.SetActive(true);
             planet_2.gameObject.SetActive(true);
             level = 2;
         }
         else if (levelDFactor < 0.5f)
         {
-
+            star1_3.gameObject.SetActive(true);
+            star2_3.gameObject.SetActive(true);
+            star3_3.gameObject.SetActive(true);
+            star4_3.gameObject.SetActive(true);
+            star5_3.gameObject.SetActive(true);
+            wave1_3.gameObject.SetActive(true);
+            wave2_3.gameObject.SetActive(true);
+            wave3_3.gameObject.SetActive(true);
+            planet_3.gameObject.SetActive(true);
+            planet_light_3.gameObject.SetActive(true);
             level = 3;
         }
         else
@@ -215,13 +299,23 @@ public class BGAnimate : MonoBehaviour
         cloud2.gameObject.SetActive(false);
         meteor1.gameObject.SetActive(false);
         meteor2.gameObject.SetActive(false);
-        star1.gameObject.SetActive(false);
+        star1_2.gameObject.SetActive(false);
         starRing.gameObject.SetActive(false);
-        star2.gameObject.SetActive(false);
-        star3.gameObject.SetActive(false);
-        star4.gameObject.SetActive(false);
+        star2_2.gameObject.SetActive(false);
+        star3_2.gameObject.SetActive(false);
+        star4_2.gameObject.SetActive(false);
         wave_2.gameObject.SetActive(false);
         planet_2.gameObject.SetActive(false);
+        star1_3.gameObject.SetActive(false);
+        star2_3.gameObject.SetActive(false);
+        star3_3.gameObject.SetActive(false);
+        star4_3.gameObject.SetActive(false);
+        star5_3.gameObject.SetActive(false);
+        wave1_3.gameObject.SetActive(false);
+        wave2_3.gameObject.SetActive(false);
+        wave3_3.gameObject.SetActive(false);
+        planet_3.gameObject.SetActive(false);
+        planet_light_3.gameObject.SetActive(false);
         stars.gameObject.SetActive(false);
     }
 }
