f4d17432247f4808d8789ebf4ee21ddd35a1bdbe
JunHanZang <zang123@unist.ac.kr>
2022-03-07 21:20:17 +0900

    backup
    dd

---
+++ Assets/GifAssets/PowerGif/Examples/Scripts/DecodeExample1.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace Assets.GifAssets.PowerGif.Examples.Scripts
+{
+	/// <summary>
+	/// Decoding GIF example.
+	/// </summary>
+	public class DecodeExample1 : ExampleBase
+	{
+		public AnimatedImage AnimatedImage;
+
+		public void Start()
+		{
+			var bytes = File.ReadAllBytes(LargeSample1);
+			var gif = Gif.Decode(bytes);
+
+			AnimatedImage.Play(gif);
+		}
+	}
+}
---
+++ Assets/GifAssets/PowerGif/Examples/Scripts/DecodeExample2.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace Assets.GifAssets.PowerGif.Examples.Scripts
+{
+	/// <summary>
+	/// Decoding GIF example.
+	/// </summary>
+	public class DecodeExample2 : ExampleBase
+	{
+		public AnimatedImage AnimatedImage;
+
+		public void Start()
+		{
+			var bytes = File.ReadAllBytes(LargeSample2);
+			var gif = Gif.Decode(bytes);
+
+			AnimatedImage.Play(gif);
+		}
+	}
+}
