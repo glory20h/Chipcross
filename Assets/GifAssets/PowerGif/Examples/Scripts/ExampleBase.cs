3023474831ca8d14b4973920fca13d57d19d227a
JunHanZang <zang123@unist.ac.kr>
2022-03-09 20:55:36 +0900

    백업
    ㅇㅇ

--- Assets/GifAssets/PowerGif/Examples/Scripts/ExampleBase.cs
+++ Assets/GifAssets/PowerGif/Examples/Scripts/ExampleBase.cs
@@ -7,7 +7,7 @@ namespace Assets.GifAssets.PowerGif.Examples.Scripts
 		protected const string SmallSample1 = "Assets/GifAssets/PowerGif/Examples/Samples/tt1.gif";
 		protected const string SmallSample2 = "Assets/GifAssets/PowerGif/Examples/Samples/tt2.gif";
 		protected const string SmallSample3 = "Assets/GifAssets/PowerGif/Examples/Samples/tt3.gif";
-		protected const string LargeSample = "Assets/GifAssets/PowerGif/Examples/Samples/tt3.gif";
+		protected const string LargeSample = "Assets/GifAssets/PowerGif/Examples/Samples/tt1.gif";
 		protected const string LargeSample1 = "Assets/GifAssets/PowerGif/Examples/Samples/tt2.gif";
 		protected const string LargeSample2 = "Assets/GifAssets/PowerGif/Examples/Samples/tt3.gif";
 		protected const string SampleFolder = "Assets/GifAssets/PowerGif/Examples/Samples";
