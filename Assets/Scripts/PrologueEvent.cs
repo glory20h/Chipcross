f1732c6e323b4aa42b2bdcc6c01c8ca948982b11
glory20h <glory20h@naver.com>
2022-05-01 23:18:04 +0900

    프롤로그 개선

--- Assets/Scripts/PrologueEvent.cs
+++ Assets/Scripts/PrologueEvent.cs
@@ -9,11 +9,31 @@ public class PrologueEvent : MonoBehaviour
     public Button display_button;
 
     int index;
+    float a_change;
+    float cum_change;
 
     // Start is called before the first frame update
     void Start()
     {
         index = 1;
+        a_change = 0.02f;
+    }
+
+    void Update()
+    {
+        display_button.GetComponent<Image>().color += new Color(0, 0, 0, a_change);
+
+        if (display_button.GetComponent<Image>().color.a > 6f)
+        {
+            display_button.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+            a_change = -a_change;
+        }
+        else if (display_button.GetComponent<Image>().color.a < -1f)
+        {
+            display_button.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+            a_change = -a_change;
+            LoadNextCut();
+        }
     }
 
     public void LoadNextCut()
@@ -27,5 +47,10 @@ public class PrologueEvent : MonoBehaviour
         {
             display_button.image.sprite = Resources.Load<Sprite>("Arts/Story/" + index.ToString());
         }
+    }
+
+    public void SkipPrologue()
+    {
+        SceneManager.LoadScene("MainBoard");
     }
 }
