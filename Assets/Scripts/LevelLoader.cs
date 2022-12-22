5a6f8ac0286030d0b99f876089a6209b9ac8ab2a
JunHanZang <zang123@unist.ac.kr>
2022-12-23 01:24:03 +0900

    gpt-chat

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -2,75 +2,60 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
 public class LevelLoader : MonoBehaviour
 {
     public Animator transition;
     public GameObject loadingScreen;
     public GameObject progressScreen;
-    [SerializeField]Image progressBar;
-    //[SerializeField]Image progressBarframe;
+    [SerializeField] Image progressBar;
     public GameObject Rocket;
     public GameObject Target;
 
     void Start()
     {
-        //MANUALLY SET STARTING DIFFICULTYFACTOR BY CHANGING THIS VALUE
-        //PlayerPrefs.SetFloat("PlayerDFactor", -1f);
-        //PlayerPrefs.SetFloat("RateChange", 4f);
+        // MANUALLY SET STARTING DIFFICULTYFACTOR BY CHANGING THIS VALUE
+        // PlayerPrefs.SetFloat("PlayerDFactor", -1f);
+        // PlayerPrefs.SetFloat("RateChange", 4f);
 
-        //MANUALLY SET STARTING TUTLEVEL BY CHANGING THIS VALUE; DEFAULT 0 -> 평소에는 주석처리 되어 있어야함
-        //PlayerPrefs.SetInt("tutorial", 1);
+        // MANUALLY SET STARTING TUTLEVEL BY CHANGING THIS VALUE; DEFAULT 0 -> 평소에는 주석처리 되어 있어야함
+        // PlayerPrefs.SetInt("tutorial", 1);
     }
 
     public void LoadNextLevel()
     {
-        if (PlayerPrefs.GetInt("tutorial", 1) == 1)
-        {
-            StartCoroutine(LoadLevel("Story"));
-        }
-        else
-        {
-            StartCoroutine(LoadLevel("MainBoard"));
-        }
+        string sceneName = (PlayerPrefs.GetInt("tutorial", 1) == 1) ? "Story" : "MainBoard";
+        StartCoroutine(LoadLevel(sceneName));
     }
 
     IEnumerator LoadLevel(string sceneName)
     {
-        yield return null;
-        //progressBar.enabled = true;
-        //progressBarframe.enabled = true;
         transition.SetTrigger("Start");
         AsyncOperation op = SceneManager.LoadSceneAsync(sceneName);
         op.allowSceneActivation = false;
 
         loadingScreen.SetActive(false);
         progressScreen.SetActive(true);
-        float timer = 0.0f; 
+
+        float timer = 0f;
         while (!op.isDone)
         {
-            yield return null; 
+            yield return null;
             timer += Time.deltaTime;
-            if (op.progress < 0.9f)
+
+            float progress = (op.progress < 0.9f) ? op.progress : 1f;
+            progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, progress, timer);
+            Rocket.transform.position = Vector3.Lerp(Rocket.transform.position, Target.transform.position, op.progress);
+
+            if (progressBar.fillAmount >= progress)
             {
-                progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, op.progress, timer);
-                //Debug.Log(op.progress);
-                Rocket.transform.position = (Target.transform.position - Rocket.transform.position) * op.progress + Rocket.transform.position;
-                //Rocket.transform.position = new Vector3(Mathf.Lerp(Rocket.transform.position.x, Target.transform.position.x, timer), Target.transform.position.y, 0);
-                if (progressBar.fillAmount >= op.progress)
-                {
-                    timer = 0f;
-                }
+                timer = 0f;
             }
-            else
+
+            if (progressBar.fillAmount >= 0.99f && op.progress >= 0.9f)
             {
-                progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, 1f, timer);
-                //Rocket.transform.position = Target.transform.position;
-                Rocket.transform.position = (Target.transform.position - Rocket.transform.position) * op.progress + Rocket.transform.position;
-                if (progressBar.fillAmount >= 0.99f)
-                {
-                    op.allowSceneActivation = true; 
-                    yield break;
-                }
+                op.allowSceneActivation = true;
+                yield break;
             }
         }
     }
