50298e7b3be762bea33fcc1dc3efc1306268332e
JunHanZang <zang123@unist.ac.kr>
2023-01-01 21:01:48 +0900

    backup
    gifs

---
+++ Assets/Scripts/GIFLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class GIFLoader : MonoBehaviour
+{
+    public IEnumerator<Texture2D> Load(string path)
+    {
+        if (path == null)
+        {
+            yield break;
+        }
+
+        // Read the GIF file into a byte array
+        byte[] bytes = File.ReadAllBytes(path);
+
+        // Create a new Texture2D and load the pixel data into it
+        Texture2D texture = new Texture2D(1, 1);
+        texture.LoadImage(bytes);
+
+        // Split the texture into a series of frames
+        int frameCount = texture.width / texture.height;
+        for (int i = 0; i < frameCount; i++)
+        {
+            // Create a new Texture2D and copy the pixel data into it
+            Texture2D frame = new Texture2D(texture.height, texture.height);
+            frame.SetPixels(texture.GetPixels(i * texture.height, 0, texture.height, texture.height));
+            frame.Apply();
+
+            // Yield the frame
+            yield return frame;
+        }
+    }
+
+}
---
+++ Assets/Scripts/GIFPlayer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GIFPlayer : MonoBehaviour
+{
+    public string GIFPath;
+    public float FrameInterval = 0.1f;
+    private GIFLoader loader;
+    private IEnumerator<Texture2D> frames;
+    private float timeSinceLastFrame;
+    private bool isPlaying;
+
+    private void Start()
+    {
+        // Create a new GIFLoader instance
+        loader = new GIFLoader();
+
+        // Load the GIF file and get the frames
+        frames = loader.Load(GIFPath);
+
+        // Set the initial time
+        timeSinceLastFrame = Time.time;
+
+        // Set isPlaying to true
+        isPlaying = true;
+    }
+
+    private void Update()
+    {
+        if (isPlaying)
+        {
+            // Check if it's time to show the next frame
+            if (Time.time - timeSinceLastFrame >= FrameInterval)
+            {
+                // Advance to the next frame
+                if (frames.MoveNext())
+                {
+                    // Get the current frame
+                    Texture2D frame = frames.Current;
+
+                    // Set the frame as the texture of the game object
+                    GetComponent<Renderer>().material.mainTexture = frame;
+
+                    // Update the time
+                    timeSinceLastFrame = Time.time;
+                }
+                else
+                {
+                    // If there are no more frames, stop playing
+                    isPlaying = false;
+                }
+            }
+        }
+    }
+
+}
