104e019dff0e2d2bba6633791fda5658fbb5b138
JunHanZang <zang123@unist.ac.kr>
2023-09-03 16:52:39 +0900

    Titlescreen : CoinFXPlayer fix

--- Assets/Scripts/CoinFXPlayer.cs
+++ Assets/Scripts/CoinFXPlayer.cs
@@ -6,11 +6,16 @@ public class CoinFXPlayer : MonoBehaviour
 {
     public static AudioClip coin;
     static AudioSource audioSrc;
-    
+    //void Awake()
+    //{
+    //    //Debug.Log("CoinFXPlayer Awake called");
+    //    // ... (기존 코드)
+    //}
     void Start()
     {
+        //DontDestroyOnLoad(gameObject);
+        
         coin = Resources.Load<AudioClip>("Audio/FX/Quick_Coin");
-
         audioSrc = GetComponent<AudioSource>();
     }
 
